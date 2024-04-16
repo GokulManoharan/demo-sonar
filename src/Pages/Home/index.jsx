3ab3cdaefd614987d576193375cd24c68caef9ef
GokulManoharan <gokool2u@gmail.com>
2024-04-16 17:38:32 +0530

    Adding home pgae

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,7 +2,14 @@ const Home = () => {
     const description = "This is the home page of the application"
 
     return (
-        <h3>{description}</h3>
+        <>
+            <h3>{description}</h3>
+            <ul>
+                <li><a>Home</a></li>
+                <li><a>About</a></li>
+                <li><a>Info</a></li>
+            </ul>
+        </>
     )
 }
 
